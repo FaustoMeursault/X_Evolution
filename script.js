ecdc9036d03d562fa52d0c58fc7447984f11c5cb
Jão <144826116+FaustoMeursault@users.noreply.github.com>
2025-08-01 11:58:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Há 3,5 bilhões a vida surgia em forma unicelular. Evolua elas até onde puder.",
         alternativas: [
             {
                 texto: "Isso é assustador!",
